a30db3efa52c8ec55792bf8b4ce40ac46e85a950
William Richard <willster3021@gmail.com>
2013-10-13 12:55:15 -0400

    Send out emails on error.

--- paxtix.py
+++ paxtix.py
@@ -1,10 +1,11 @@
 import os
 import sys
 from argparse import ArgumentParser
+import traceback
+from automail.automail import parse_settings, send_email
 sys.path.append(os.path.join(os.path.dirname(__file__), 'tweepy'))
 from tweepy.auth import OAuthHandler
 from tweepy.streaming import Stream, StreamListener
-from automail import automail
 import re
 
 
@@ -21,6 +22,9 @@ def parse_oauth_file(oauthFileLoc):
 
 class PaxListener(StreamListener):
 
+    def __init__(self, mail_settings_loc):
+        StreamListener.__init__(self)
+        self.mail_settings = parse_settings(mail_settings_loc)
     '''
     uncomment this if you want to see what a 'status' object will look like
     '''
@@ -34,42 +38,52 @@ class PaxListener(StreamListener):
         if re.search(r'east|ticket|tickets|registration|open', status.text, re.IGNORECASE): #regex open for editing
             subject = "New message from %s" % status.user.name.encode('ascii', 'xmlcharrefreplace')
             message = status.text.encode('ascii', 'xmlcharrefreplace')
-            automail.send_email(automail.parse_settings("mail_settings.txt"),
-                                subject, message)
+            send_email(self.mail_settings, subject, message)
         return True
 
-    def on_error(self, status):
-        print status
-        return False
+    def on_error(self, status_code):
+        subject = status_code + "error recieved by tweepy!"
+        message = "PAX tix listener recieved a " + status_code + " error from twitter"
+        send_email(self.mail_settings, subject, message)
+        return True
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("oauth_keys_file",
-                    help="The location of a file containing the application " +
-                    "oath consumer key, consumer secret, " +
-                    "access token, and access token secret, " +
-                    "each on its own line, in that order.  " +
-                    "See the tweepy example on oauth to figure " +
-                    "out how to get these keys."
-                    )
-    args = parser.parse_args()
-    (consumer_key, consumer_secret, access_token, access_token_secret) = \
-        parse_oauth_file(args.oauth_keys_file)
+    try:
+        parser = ArgumentParser()
+        parser.add_argument("oauth_keys_file",
+                        help="The location of a file containing the application " +
+                        "oath consumer key, consumer secret, " +
+                        "access token, and access token secret, " +
+                        "each on its own line, in that order.  " +
+                        "See the tweepy example on oauth to figure " +
+                        "out how to get these keys."
+                        )
+        parser.add_argument("mail_settings_file", help="The automail settings file for sending emails.")
+        args = parser.parse_args()
+        (consumer_key, consumer_secret, access_token, access_token_secret) = \
+            parse_oauth_file(args.oauth_keys_file)
+    
+        pl = PaxListener(args.mail_settings_file)
+    
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+    
+        #open a stream, with ourself as the listener
+        stream = Stream(auth, pl)
+        #stream.filter(track=["the"])
+    
+        pax_user_id = '26281970' #follow requires userid, found at mytwitterid.com
+        stream.filter(follow=['1954653840']) #track ignores follow, pulls from firehose regardless (this is testing acct)
 
-    pl = PaxListener()
-
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    #open a stream, with ourself as the listener
-    stream = Stream(auth, pl)
-    #stream.filter(track=["the"])
-
-    pax_user_id = '26281970' #follow requires userid, found at mytwitterid.com
-    stream.filter(follow=['1954653840']) #track ignores follow, pulls from firehose regardless (this is testing acct)
-
-
+    except BaseException as e:
+        subject = "Exception from paxtix"
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        message = '\n'.join(["Pax tix listener has hit an exception!",] + 
+                             traceback.format_exception(exc_type, exc_value, exc_traceback),
+                             )
+        send_email(parse_settings(args.mail_settings_file), subject, message)
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
 
 if __name__ == "__main__":
     main()
